ed6cb692a6fba25033c0746a0241f3a1ecae68af
CollectionofEG <148367269+Pepperhfj@users.noreply.github.com>
2025-05-12 21:35:05 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function change() {
+    document.getElementById("nate").innerHTML = 
+    "<p style='font-size: 50px;'>CHICKEN ASS!!!</p> <img src='https://media.tenor.com/_J1uK4Xrnq8AAAAi/rofl-emoji-man-rofl.gif'>";
+    }
