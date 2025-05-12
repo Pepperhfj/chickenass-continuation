28d2504274671ee503a89cb10ed4dc74d9efc377
CollectionofEG <148367269+Pepperhfj@users.noreply.github.com>
2025-05-12 21:45:45 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function change() {
     document.getElementById("nate").innerHTML = 
-    "<p style='font-size: 50px;'>CHICKEN ASS!!!</p> <img src='https://media.tenor.com/_J1uK4Xrnq8AAAAi/rofl-emoji-man-rofl.gif'>";
+    "<p style='font-size: 50px; color: black;'>CHICKEN ASS!!!</p> <img src='https://media.tenor.com/_J1uK4Xrnq8AAAAi/rofl-emoji-man-rofl.gif'>";
     }
